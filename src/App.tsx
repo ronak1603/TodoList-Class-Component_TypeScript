2d183f16596325f3f73c55b303fdc84a8a2e926c
ronak1603 <ronakmalani.cse19@chitkarauniversity.edu.in>
2022-08-18 15:33:43 +0530

    Route added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,40 @@ import "./App.css";
 import React from "react";
 import TodoHeading from "./Components/TodoHeading/TodoHeading";
 import TaskList from "./Components/TaskList/TaskList";
+import {
+  BrowserRouter as Router
+  , Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 class App extends React.Component {
   render() {
     return (
-        <div className="App">
-          <TodoHeading />
-          <TaskList />
-        </div>
+      <div className="App">
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/todo">Todo</Link>
+                </li>
+              </ul>
+            </nav><Routes>
+              <Route path="/todo" element={<><TodoHeading /><TaskList /></>}>
+              </Route>
+              <Route path="/" element={<h1>Home Page</h1>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+        {/* <TodoHeading />
+          <TaskList /> */}
+      </div>
     );
   }
 }
